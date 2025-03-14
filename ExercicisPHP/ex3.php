494e09b701fb75faa1b75fd4a2588a4dc12dc8cb
Adrià Sbert Dasilva <adria.sbert.7e8@itb.cat>
2025-03-14 07:33:06 +0000

    ex

--- ExercicisPHP/ex3.php
+++ ExercicisPHP/ex3.php
@@ -1,6 +1,16 @@
 //EXERCICI 3
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-    for($i = 0; $i <10; $i++){
-        echo" 7 x $i = ". (7*$i);
+    for($i = 0; $i <=10; $i++){
+        echo "<br> 7 x $i = ". (7*$i). "</br>";
     }
 ?>
+</body>
+</html>
--- ExercicisPHP/ex4.php
+++ ExercicisPHP/ex4.php
@@ -1,5 +1,16 @@
+//EXERCICI 4
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-    for($num = 0; $num < 10; $num++){
-        echo"<a href='https://es.wikipedia.org/wiki/$num' target='_blank'>$num</a>";
+    for($num = 0; $num <=10; $num++){
+        echo "$num -> <a href='https://es.wikipedia.org/wiki/$num'> https://es.wikipedia.org/wiki/$num</a><br>";
     }
 ?>
+</body>
+</html>
